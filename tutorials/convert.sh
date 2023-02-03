ebeee8672114acfccf3146f65f499836e70639d8
Brian Johnson <brianjo@fb.com>
2023-02-03 17:03:44 -0500

    Update convert.sh

--- tutorials/convert.sh
+++ tutorials/convert.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 for filename in ./*.ipynb; do
 jupyter nbconvert "$filename" --to markdown --execute --output "$(basename "$filename" .ipynb).md"
-cp "$(basename "$filename" .ipynb).md" ../docs/
+# cp "$(basename "$filename" .ipynb).md" ../docs/
+cp -r . ../docs/
 done
